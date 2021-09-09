7d57b7585e78e8063090363314587c98c2be82d9
shlokgupta26 <90414000+shlokgupta26@users.noreply.github.com>
2021-09-09 23:29:34 +0530

    my first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<long long int>
+#define endl "\n"
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    {
+       cout<<"Hello World";
+
+  }
+
+    return 0;
+}
+
